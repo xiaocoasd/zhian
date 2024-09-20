fb42681442acf0bb55ab8ba2194bb8f6103c7e5f
xiaocoasd <134391783+xiaocoasd@users.noreply.github.com>
2024-09-20 12:41:08 +0800

    Delete adv_test directory

--- adv_test/env_adv/env_adv_map/__init__.py
+++
@@ -1,7 +0,0 @@
-from adv_test.env_adv.env_adv_map.env_adv_CartPole import EnvCartPoleAdv
-from adv_test.env_adv.env_adv_map.env_adv_Highway import EnvHighwayAdv
-
-__all__ = [
-    "EnvCartPoleAdv",
-    "EnvHighwayAdv",
-]
--- adv_test/show/__init__.py
+++
@@ -1,7 +0,0 @@
-from adv_test.show.logits_plt import LogitsPlt
-from adv_test.show.tree import TreeTop
-
-__all__ = [
-    "LogitsPlt",
-    "TreeTop",
-]
