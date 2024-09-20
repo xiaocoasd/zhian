340f6f06f4e0ab9453f659faebd45ac5a698b0d5
xiaocoasd <134391783+xiaocoasd@users.noreply.github.com>
2024-09-20 12:28:35 +0800

    Delete adv_test directory

--- adv_test/adv_sample_gene/__init__.py
+++
@@ -1,7 +0,0 @@
-from adv_test.adv_sample_gene.algo_utils import make_img_atk
-from adv_test.adv_sample_gene.algo_top import ImgAdvObsGene
-
-__all__ = [
-    "make_img_atk",
-    "ImgAdvObsGene",
-]
--- adv_test/execute/__init__.py
+++
@@ -1,7 +0,0 @@
-from adv_test.execute.execute_with_adv import TestWithAdv
-from adv_test.execute.execute_per_epoch import ExecutePerEpoch
-
-__all__ = [
-    "TestWithAdv",
-    "ExecutePerEpoch",
-]
